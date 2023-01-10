0d9d37596b4e789fb8891b0edd8815f0565419f0
Stergios Zissakis <stergios.zissakis@accelligence.tech>
2023-01-10 22:27:45 +0000

    VPN. Oddsportal start

--- Browser.py
+++ Browser.py
@@ -11,6 +11,9 @@ class Browser:
         self.chrome_options.add_argument('--start-maximized')
         self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.chrome_options.add_argument('disable-gpu')
+        self.chrome_options.add_argument("--incognito")
+        self.chrome_options.add_experimental_option("detach", True)
+        self.chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
         #self.chrome_options.add_argument('--headless')
 
         self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+import time
 from Browser import Browser
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+def sleep_for_millis(millis):
+    time.sleep(millis / 1000)
+
+def scroll_to_visible(driver, element):
+    #actions = ActionChains(driver)
+    #actions.move_to_element(element).perform()
+    driver.execute_script("arguments[0].scrollIntoView(true);", element);
+    sleep_for_millis(300)
+
+def accept_cookies(driver, button_text):
+    button = driver.find_element(By.XPATH, "//button[text()='" + button_text + "']")
+    button.click()
+    sleep_for_millis(200)
 
 def main():
     browser = Browser()
-    page = browser.get("https://www.google.com/")
-    print(page.title)
+    page = browser.get("https://www.oddsportal.com/")
+    
+    accept_cookies(page, "I Accept")
+
+    soccer_link = page.find_element(By.XPATH, "//p[text()='soccer']")
+    scroll_to_visible(page, soccer_link)
+    soccer_link.click()
+
+    sleep_for_millis(400)
+    greece_link = page.find_element(By.XPATH, "//h2[text()='Greece']")
+    scroll_to_visible(page, greece_link)
+    greece_link.click()
 
 if __name__ == "__main__":
     main()
