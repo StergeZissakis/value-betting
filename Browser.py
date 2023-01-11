2e9041a4bb80f5743f63b8c506eee9f0b14903d0
Stergios Zissakis <stergios.zissakis@accelligence.tech>
2023-01-11 23:21:10 +0000

    Intermediate

--- Browser.py
+++ Browser.py
@@ -1,6 +1,8 @@
+import pickle
+import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 
 class Browser:
 
@@ -11,13 +13,25 @@ class Browser:
         self.chrome_options.add_argument('--start-maximized')
         self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.chrome_options.add_argument('disable-gpu')
-        self.chrome_options.add_argument("--incognito")
+        #self.chrome_options.add_argument("--incognito")
         self.chrome_options.add_experimental_option("detach", True)
         self.chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
         #self.chrome_options.add_argument('--headless')
 
         self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
-    
+
+        # Load cookies from previous session
+        #'''
+        self.cookies_file = "./config/chrome.cookies.pkl"
+        if(os.path.isfile(self.cookies_file)):
+            cookies = pickle.load(open(self.cookies_file, "rb"))
+            for cookie in cookies:
+                self.driver.add_cookie(cookie)
+        #'''
+
+    def __del__(self):
+        # Save cookies for next session
+        pickle.dump(self.driver.get_cookies(), open(self.cookies_file, "wb"))
 
     def get(self, url):
         self.page = self.driver.get(url)
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def sleep_for_millis(millis):
 def scroll_to_visible(driver, element):
     #actions = ActionChains(driver)
     #actions.move_to_element(element).perform()
-    driver.execute_script("arguments[0].scrollIntoView(true);", element);
+    driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", element);
     sleep_for_millis(300)
 
 def accept_cookies(driver, button_text):
@@ -21,16 +21,38 @@ def main():
     browser = Browser()
     page = browser.get("https://www.oddsportal.com/")
     
+    # Click I Accept
     accept_cookies(page, "I Accept")
 
+    #Find soccer
     soccer_link = page.find_element(By.XPATH, "//p[text()='soccer']")
     scroll_to_visible(page, soccer_link)
     soccer_link.click()
 
+    #Find Greece
     sleep_for_millis(400)
     greece_link = page.find_element(By.XPATH, "//h2[text()='Greece']")
     scroll_to_visible(page, greece_link)
     greece_link.click()
+
+    #Visit all links under Greece
+    ## Get all the attributes
+    dataElement = None
+    attribs = greece_link.get_property('attributes')[0].keys()
+    for attrib in attribs:
+        if attrib.startswith('data-v-'):
+            dataElement=attrib
+            break
+
+    if dataElement:
+        print(dataElement)
+
+        top_href = greece_link.find_element_by_xpath('..').find_element_by_xpath('..') #find the top <a href
+        print(top_href)
+
+        ul = top_href.find_element_by_xpath('//a[@href]/ul[@' + dataElement + ']')
+        print(ul)
+
 
 if __name__ == "__main__":
     main()
