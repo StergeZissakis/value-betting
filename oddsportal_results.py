1d5d78550788fa93709229cfb1356c29919f387c
Stergios Zissakis <stergios.zissakis@accelligence.tech>
2023-02-21 23:19:23 +0200

    Results before collection and insersion

--- oddsportal_results.py
+++ oddsportal_results.py
@@ -56,7 +56,7 @@ def get_event_time(section_div, kind):
     if kind  == "Today":
         div = section_div.find_element(By.XPATH, './div[3]/div/a/div/div/p')
     elif kind == "Yesterday":
-        div = section_div.find_element(By.XPATH, './div[2]/div/a/div/div/p')
+        div = section_div.find_element(By.XPATH, './div[3]/div/a/div/div/p')
     elif kind == "Date":
         div = section_div.find_element(By.XPATH, './div[2]/div/a/div/div/p')
     elif kind == "Match":
@@ -69,16 +69,35 @@ def get_event_time(section_div, kind):
 
 def process_results(db, browser, page):
     container_div = page.find_element(By.XPATH, '//*[@id="app"]/div/div[1]/div/main/div[2]/div[7]/div[1]')
+    #load all the content
     section_divs = container_div.find_elements(By.XPATH, './div[@set="65147"]')
-    print(len(section_divs))
+    browser.scroll_to_bottom()
+    browser.scroll_to_visible(section_divs[0])
+
+    # and then hit it!
+    section_divs = container_div.find_elements(By.XPATH, './div[@set="65147"]')
     event_date = None
-    for section in section_divs:
-        kind = get_section_kind(section)
-        if kind is not None:
-            event_date = get_event_date(section, event_date, kind) 
-            event_time = get_event_time(section, kind)
-            event_date_time = browser.add_time_to_date(event_date, event_time)
-            print(event_date_time)
+    num_sections = len(section_divs)
+    processed_sections = 0;
+    print("Num:" + str(num_sections))
+    print("Prc:" + str(processed_sections))
+    while processed_sections < num_sections:
+        for section in section_divs:
+            kind = get_section_kind(section)
+            if kind is not None:
+                browser.scroll_to_visible(section)
+                browser.sleep_for_millis_random(300)
+                event_date = get_event_date(section, event_date, kind) 
+                event_time = get_event_time(section, kind)
+                event_date_time = browser.add_time_to_date(event_date, event_time)
+                print(event_date_time)
+
+        section_divs = container_div.find_elements(By.XPATH, './div[@set="65147"]')
+        processed_sections += num_sections
+        section_divs = section_divs[processed_sections:-1]
+        num_sections += len(section_divs)
+        print("Num:" + str(num_sections))
+        print("Prc:" + str(processed_sections))
 
 
 
@@ -93,7 +112,4 @@ if __name__ == "__main__":
     # Click I Accept
     browser.accept_cookies("//button[text()='I Accept']")
 
-    browser.scroll_to_bottom()
-    paginator = page.find_element(By.XPATH, '//div[@id="pagination"]')
-    browser.scroll_move_left_click(paginator.find_element(By.XPATH, './span'))
     process_results(db, browser, page)
