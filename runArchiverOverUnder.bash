4af81836dec9633e1df726c80a9a98d3a23dccb6
Stergios Zissakis <stergios.zissakis@accelligence.tech>
2023-02-26 10:29:19 +0200

    Better archiver script. New DB backup

--- runArchiverOverUnder.bash
+++ runArchiverOverUnder.bash
@@ -2,7 +2,7 @@
 
 source python_env/bin/activate
 
-psql -h localhost -U postgres -c 'SELECT "ArchivePastMatches"();' 2>&1 | tee logs/ArchivePastMatches.log
+psql -h localhost -U postgres -c 'SELECT "ArchivePastMatches"();' 2>&1 | tee -a logs/ArchivePastMatches.log
 
 exec 3< <(cd config; sudo ./connect_italy.sudo 2>&1 | tee logs/italy_vpn_archiver.log)
 sed '/Initialization Sequence Completed$/q' <&3 ; cat <&3 &
@@ -13,3 +13,6 @@ time python ./oddsportal_results.py 2>&1 | tee logs/oddsportal_results.log
 while [ $? != 0 ]; do
     time python ./oddsportal_results.py 2>&1 | tee -a logs/oddsportal_results.log  
 done
+
+
+psql -h localhost -U postgres -c 'SELECT "CalculateOverUnderResults"();' 2>&1 | tee -a logs/ArchivePastMatches.log
