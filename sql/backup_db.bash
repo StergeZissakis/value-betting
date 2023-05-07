968ff69946104a8fc88b46bb65ba138826db53ae
Stergios Zissakis <stergios.zissakis@accelligence.tech>
2023-05-07 19:25:13 +0300

    Data dumo in sql insert format

--- sql/backup_db.bash
+++ sql/backup_db.bash
@@ -2,5 +2,5 @@
 
 pg_dump --blobs --clean  --create --encoding=UTF-8 --file=postgresql.sql --format=plain --no-owner --superuser=postgres --verbose --if-exists --dbname=postgres --port=5432 --username=postgres 
 pg_dump --schema-only --blobs --clean  --create --encoding=UTF-8 --file=definitions.sql --format=plain --no-owner --superuser=postgres --verbose --if-exists --dbname=postgres --port=5432 --username=postgres 
-pg_dump --data-only --blobs --create --encoding=UTF-8 --file=data.sql --format=plain --no-owner --superuser=postgres --verbose --dbname=postgres --port=5432 --username=postgres 
+pg_dump --data-only --column-inserts --blobs --encoding=UTF-8 --file=data.sql --format=plain --no-owner --superuser=postgres --verbose --dbname=postgres --port=5432 --username=postgres 
 
